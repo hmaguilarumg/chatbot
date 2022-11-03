373812fd507113af1ccbb246515176b76f9bd58b
hmaguilarumg <85041170+hmaguilarumg@users.noreply.github.com>
2022-11-02 21:16:23 -0600

    pruebasdesdeexamen

--- correo/enviarcorreo.js
+++ correo/enviarcorreo.js
@@ -7,7 +7,7 @@ const msg = {
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  templateId:"d-742487d99b0f4e798bcc606920b762e3" 
+  templateId:"d-742487d99b0f4e798bcc606920b762e3" .
 }
 sgMail
   .send(msg)
